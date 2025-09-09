97e025173225009d5788bc4966fcaa5a2e2277bb
prazumov <subgreen75@yandex.ru>
2025-09-09 14:35:30 +0500

    Финальный проект. Часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,9 @@
-import model.Book;
-import model.User;
 import service.Library;
 import ui.ConsoleMenu;
 
-import java.util.HashMap;
-
 public class Main {
-
-
     public static void main(String[] args) {
-
-
-
         Library.init();
-        /*
-        Library.displayBooks(Library.books);
-        HashMap<Integer, Book> booksFind;
-        booksFind = Library.getBooks(1,null,null,0);
-        Library.displayBooks(booksFind);
-        booksFind = Library.getBooks(0,"ВИ",null,0);
-        Library.displayBooks(booksFind);
-        booksFind = Library.getBooks(0,"","Толс",0);
-        Library.displayBooks(booksFind);
-
-        HashMap<Integer, User> usersFind;
-        usersFind = Library.getUsers(0,null,"mail");
-        Library.displayUsers(usersFind);
-*/
         ConsoleMenu.start();
     }
-
-
 }
